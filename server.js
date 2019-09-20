c5108fb1c2adc9e22bd6c10fcd755e903d73a970
Ben Grabowski <gbowski30@gmail.com>
2019-09-20 08:44:35 -0600

    pokedex.json not included in intial commit. adding now

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const POKEDEX = require('./pokedex.json')
-
+const POKEDEX = require('./')
 const app = express()
 
 app.use(morgan('dev'))
